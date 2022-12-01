6bbc62df848f81df1ffe80f42f3a8fa0798d84ec
Siru <ehdgur9553@naver.com>
2022-12-02 01:48:50 +0900

    final -> change the button color-design

--- calculator_main.py
+++ calculator_main.py
@@ -12,20 +12,32 @@ class Main(QDialog):
         self.init_ui()
 
     def init_ui(self):
-        
         main_layout = QVBoxLayout()
 
         ### 각 위젯을 배치할 레이아웃을 미리 만들어 둠
         layout_button = QGridLayout()
         layout_equation_solution = QGridLayout()
-        # layout_equation_solution = QFormLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
         self.equation = QLineEdit("")
-        # self.solution = QLineEdit("")
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
         layout_equation_solution.addWidget(self.equation, 0, 0)
+
+        ### =, clear, backspace 버튼 생성
+        button_equal = QPushButton("=")
+        button_clear = QPushButton("C")
+        button_backspace = QPushButton("<-")
+
+        ### =, clear, backspace 버튼 클릭 시 시그널 설정
+        button_equal.clicked.connect(self.button_equal_clicked)
+        button_clear.clicked.connect(self.button_clear_clicked)
+        button_backspace.clicked.connect(self.button_backspace_clicked)
+
+        ### =, clear, backspace 버튼을 layout_button 레이아웃에 추가
+        layout_button.addWidget(button_equal, 5, 3)
+        layout_button.addWidget(button_clear, 0, 2)
+        layout_button.addWidget(button_backspace, 0, 3)
 
         ### 사칙연상 버튼 생성
         button_plus = QPushButton("+")
@@ -44,21 +56,6 @@ class Main(QDialog):
         layout_button.addWidget(button_minus, 3, 3)
         layout_button.addWidget(button_product, 2, 3)
         layout_button.addWidget(button_division, 1, 3)
-
-        ### =, clear, backspace 버튼 생성
-        button_equal = QPushButton("=")
-        button_clear = QPushButton("C")
-        button_backspace = QPushButton("<-")
-
-        ### =, clear, backspace 버튼 클릭 시 시그널 설정
-        button_equal.clicked.connect(self.button_equal_clicked)
-        button_clear.clicked.connect(self.button_clear_clicked)
-        button_backspace.clicked.connect(self.button_backspace_clicked)
-
-        ### =, clear, backspace 버튼을 layout_button 레이아웃에 추가
-        layout_button.addWidget(button_clear, 0, 2)
-        layout_button.addWidget(button_backspace, 0, 3)
-        layout_button.addWidget(button_equal, 5, 3)
 
         ### %, CE, C, 1/x, 제곱, 제곱근 버튼 생성
         button_percent = QPushButton("%")
@@ -132,7 +129,6 @@ class Main(QDialog):
         global tempOperation
         tempFigure2 = self.equation.text()
         tempFigure2 = float(tempFigure2)
-        print(tempOperation)
         if tempOperation == "+":
             solution = tempFigure1 + tempFigure2
             self.equation.setText(str(solution))
@@ -183,6 +179,10 @@ class Main(QDialog):
         equation = self.equation.text()
         equation = float(equation) ** (1/2)
         self.equation.setText(str(equation))
+
+    def check_dot(self):
+        equation = self.equation.text()
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
